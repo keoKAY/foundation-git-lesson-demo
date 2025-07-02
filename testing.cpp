f41d7fd55fdd6dbd0d32ab74501870c3857ee386
keoKAY <keokay888@gmail.com>
2025-07-02 04:04:55 +0000

    adding student class

--- testing.cpp
+++ testing.cpp
@@ -1,5 +1,11 @@
 #include<iostream> 
-using namespace std; 
+using namespace std;
+class Student{
+    private: 
+        int id; 
+        string name; 
+        string classroom; 
+} ; 
 int main(){
     cout<<"This is just a testing code "<<endl; 
     return 0 ; 
