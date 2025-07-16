b5a7885809ed0740eee56b899db6eeb74e0c34a1
kk <lyvanna544@gmail.com>
2025-07-16 09:47:30 +0700

    features: adding Worker and SalaryCal from the reviews

--- worker.cpp
+++ worker.cpp
@@ -1,6 +1,18 @@
 #include<iostream> 
 using namespace std; 
 
+class SalaryCalculator{
+    private: 
+        float wage; 
+        float hour; 
+};
+class Worker{
+    public: 
+        int workerId; 
+        string workerName; 
+        string gender; 
+    
+}; 
 int main(){
     system("clear"); 
     cout<<"This is the worker code "
