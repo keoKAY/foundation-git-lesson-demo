12b7dfab5c29270ecd5c2448b0c9071543549248
sexymanalive <lyvanna544@gmail.com>
2025-07-16 02:49:54 +0000

    features: adding worker class

--- worker.cpp
+++ worker.cpp
@@ -1,8 +1,15 @@
 #include<iostream> 
 using namespace std; 
 
+class Worker{
+    private: 
+        int id; 
+        string workerName; 
+        string gender; 
+};
 int main(){
     system("clear"); 
+    cout<<"Hello from the worker "; 
     cout<<"This is the worker code "
     <<endl; 
     return 0; 
