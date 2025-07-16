411b10c3443cad62877bfa5aa6d912c50431b516
sexymanalive <lyvanna544@gmail.com>
2025-07-16 01:33:42 +0000

    modified thestudent files

--- student.cpp
+++ student.cpp
@@ -1,6 +1,18 @@
 #include<iostream> 
 using namespace std; 
 
+class Student {
+    private: 
+        int id; 
+        string name; 
+        string gender; 
+    public: 
+        Student(int id; string name; string gender ){
+            this-> id  = id;
+            this-> name  = name; 
+            this-> gender = gender; 
+        }
+}; 
 int main(){
     cout<<"Hello world"<<endl; 
     return 0; 
